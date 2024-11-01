476736c48934737363888d8aee621a0b3aec948c
sabamahmood4 <sm12882@nyu.edu>
2024-11-01 15:15:38 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -67,7 +67,7 @@ dns_records = {
     'yahoo.com.': {dns.rdatatype.A: '192.168.1.105'},
     'nyu.edu.': {
     dns.rdatatype.A: '192.168.1.106',
-    dns.rdatatype.TXT: (str(encrypted_value),),  # Ensure this contains the valid encrypted string
+    dns.rdatatype.TXT:str(encrypted_value),  # Ensure this contains the valid encrypted string
     dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
     dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
     dns.rdatatype.NS: 'ns1.nyu.edu.',
