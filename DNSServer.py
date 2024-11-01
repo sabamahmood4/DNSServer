d51a9d562698e1f16d12461f0fcf0b802149f1b3
sabamahmood4 <sm12882@nyu.edu>
2024-11-01 15:35:55 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -73,7 +73,7 @@ dns_records = {
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Using UDP
-    server_socket.bind(('127.0.0.1', 5353))  # Binding to localhost on port 5353
+    server_socket.bind(('127.0.0.1', 53))  # Binding to localhost on port 5353
 
     while True:
         try:
